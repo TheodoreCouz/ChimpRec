eba829779df174d2554401800761f5ccef674dc7
JulienDemeure <julien.demeure.2002@gmail.com>
2025-02-11 13:43:32 +0100

    modif du fichier de séparation frame par frame

--- Code/separate_video.py
+++ Code/separate_video.py
@@ -1,9 +1,12 @@
 import cv2
 import os
+import shutil
 
-def extract_frames(video_path, output_folder):
+def extract_frames(video_path, output_folder, frame_hop=1):
     filename = video_path.split("/")[-1].split(".")[0]
-    print(filename)
+    
+    if os.path.exists(output_folder):
+        shutil.rmtree(output_folder)
     
     os.makedirs(output_folder)
     cap = cv2.VideoCapture(video_path)
@@ -19,12 +22,13 @@ def extract_frames(video_path, output_folder):
         if not ret:
             break  
         
-        frame_filename = os.path.join(output_folder, f"{filename}_frame_{frame_count:04d}.png")
-        cv2.imwrite(frame_filename, frame)
+        if ((frame_count % frame_hop) == 0): 
+            frame_filename = os.path.join(output_folder, f"{filename}_frame_{frame_count:04d}.png")
+            cv2.imwrite(frame_filename, frame)
         frame_count += 1
     
     cap.release()
 
-video_path = './video_chimp/penda1.mp4'  # Remplace avec le chemin de ta vidéo
+video_path = './Chimprec Dataset/Chimp_video/Identification/Individuelle/Lwama - LM/Lwama1.mp4'  # Remplace avec le chemin de ta vidéo
 output_folder = 'separated_video_folder'  # Dossier où enregistrer les frames
 extract_frames(video_path, output_folder)
