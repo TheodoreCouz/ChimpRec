1d8b755439a347fb5961c3a7a22cf1decae03a15
JulienDemeure <julien.demeure.2002@gmail.com>
2025-04-02 17:02:09 +0200

    Fonctions pour le tracking + mise au propre de la reconnaissance avec FaceNet

--- Code/Tracking/user_interaction/ui_lib.py
+++ Code/Tracking/user_interaction/ui_lib.py
@@ -1,3 +1,19 @@
+import sys
+import os
+import cv2
+import numpy as np
+from torchvision import transforms
+from PIL import Image
+import torch
+import torchreid
+from ultralytics import YOLO
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+from deep_sort.deep_sort.detection import Detection
+from deep_sort.deep_sort.tracker import Tracker as DeepSortTracker
+from deep_sort.deep_sort import nn_matching
+
 class raw_tracking_data_reader():
     """
     This class reads the output file generated 
@@ -89,18 +105,144 @@ def edit_raw_output(RTD_reader, S1M_reader):
 
     return modified_data
 
-def draw_bbox_from_file(file_path, input_video_path, output_video_path):
+def draw_bbox(image, color, bbox, label):
+    x1, y1, x2, y2 = map(lambda v: int(float(v)), bbox)
+    factor = 0.65 if label == "Face" else 0.3
+    font_scale = max(0.4, (x2 - x1 + y2 - y1) / 300) * factor
+
+    cv2.rectangle(image, (x1, y1), (x2, y2), color, 4)
+    label_text = f"{label}"#: {score:.2f}"
+    (w, h), _ = cv2.getTextSize(label_text, cv2.FONT_HERSHEY_COMPLEX, font_scale, 1)
+
+    overlay = image.copy()
+    cv2.rectangle(overlay, (x2 - w - 10, y2 - h - 10), (x2, y2), color, -1)
+    cv2.addWeighted(overlay, 0.5, image, 0.5, 0, image)
+
+    cv2.putText(image, label_text, (x2 - w - 5, y2 - 5), cv2.FONT_HERSHEY_COMPLEX, font_scale, (255,255,255), 1)
+    return image
+
+def draw_bbox_from_file(file_path, input_video_path, output_video_path, colors):
     """
     draw the bounding boxes and class_id contained in <file_path>
     On the input video located at <input_video_path>
     The output video must be saved at <output_video_path>
     """
+    cap = cv2.VideoCapture(input_video_path)
+    ret, frame = cap.read()
 
-def perform_tracking(input_video_path, output_text_file_path):
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
+
+    file_improve_tracking = open(file_path, "r")
+    bbox_per_frame = file_improve_tracking.read().split("#")
+    colors_used = {}
+    frame_number = 0
+    while ret: 
+        #print(frame_number)
+        bboxes = bbox_per_frame[frame_number].split("\n")
+        if len(bboxes) == 1 and bboxes[0] == '': 
+            frame_number+=1
+            continue
+        
+        id_in_frame = []
+        for id_bbox in bboxes: 
+            if id_bbox == '': continue
+            id, x1, y1, x2, y2 = id_bbox.split(" ")
+            bbox = x1, y1, x2, y2
+            id_in_frame.append(id)
+            if id not in colors_used.keys(): 
+                colors_used[id] = colors.pop(0) 
+            
+            draw_bbox(frame, colors_used[id], bbox, f"Chimp n°{id}")
+
+        for id in list(colors_used.keys()):
+            if id not in id_in_frame: 
+                colors.append(colors_used[id])
+                colors_used.pop(id)
+
+        out.write(frame)
+
+        frame_number += 1
+        ret, frame = cap.read()
+    file_improve_tracking.close()
+
+def extract_features_osnet(frame, bbox, model_feature_extraction):
+    """Extrait les features d'un chimpanzé avec OSNet (permet à DeepSORT d'utiliser aussi l'apparence de l'objet en plus de la position, ...)"""
+    transform = transforms.Compose([
+        transforms.Resize((256, 128)),  # Taille attendue par OSNet
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    x1, y1, x2, y2 = map(int, bbox)  # Convertir bbox en entiers
+    chimp_img = frame[y1:y2, x1:x2]  # Extraire la région d'intérêt
+
+    chimp_img = cv2.cvtColor(chimp_img, cv2.COLOR_BGR2RGB)  # Convertir en RGB
+    chimp_img = transform(Image.fromarray(chimp_img)).unsqueeze(0)  # Appliquer transformations
+
+    with torch.no_grad():
+        feature = model_feature_extraction(chimp_img)  # Extraire les features
+
+    # Convertir les features en vecteur 1D et s'assurer qu'ils ont une forme correcte
+    feature = feature.cpu().numpy().flatten()  # Retourner un vecteur 512D
+    return feature
+
+def perform_tracking(input_video_path, output_text_file_path, detection_model, tracker, confidence_threshold, model_feature_extraction):
     """
     Performs the tracking on <input_video_path>
     Saves the bounding boxes metadata whithin <output_text_file_path>
     """
+    cap = cv2.VideoCapture(input_video_path)
+    ret, frame = cap.read()
+    file_improve_tracking = open(output_text_file_path, "w")
+
+    frame_number = 0
+    while ret: 
+        print(frame_number)
+        file_improve_tracking.write("#\n")
+        predictions = detection_model.predict(frame, verbose=False)[0]
+        
+        #On ne garde que les détections de bboxes qui ont une confiance suppérieur à 0.2
+        detections = []
+        for x1, y1, x2, y2, score, _ in predictions.boxes.data.tolist():
+            if score >= confidence_threshold:
+                bbox = np.array([x1, y1, x2 - x1, y2 - y1]) #Coin supérieur gauche + width et height 
+                confidence = float(score)
+                #feature permet à DeepSORT de prendre en compte l'apparence en plus dans le tracking donc il faudrait extraire l'apparence grâce à un model 
+                #Fonctionne un peu comme SORT pour l'instant ducoup car l'apparence est pas prise en compte 
+                feature = extract_features_osnet(frame, [x1, y1, x2, y2], model_feature_extraction) 
+                detections.append(Detection(bbox, confidence, feature))
+
+        # Mettre à jour avec les détections et forcer l'association des features
+        if len(detections) > 0:
+            tracker.predict()  # Prédit la position des objets dans la frame suivante
+            tracker.update(detections)  # Maj avec les détections
+
+
+        #Noter dans le fichier txt les bboxes et les ids correspondants
+        for track in tracker.tracks: #Parcourt les objets suivis
+            # Check que l'objet est fiable (pas un objet incertain ou qui vient d'apparaître) + check si l'objet a déjà des features d'apparence enregistrées
+            if track.is_confirmed() and track.track_id not in tracker.metric.samples:
+                tracker.metric.samples[track.track_id] = [] #pour stocker les caractéristiques d'apparence calculées grâce à OSNet
+
+            #Si pour un objet suivi il y a des doutes sur son identité ou si pas été mis à jour depuis trop de temps, on l'ignore.
+            if not track.is_confirmed() or track.time_since_update > 1: 
+                continue
+
+            bbox = track.to_tlbr()  # Format [x1, y1, x2, y2]
+            track_id = track.track_id
+
+            # Ecrire la bounding box avec l'ID du chimpanzé
+            str_bbox = ' '.join(map(str, bbox))
+            file_improve_tracking.write(f"{track_id} {str_bbox}\n")
+
+        ret, frame = cap.read()
+        frame_number += 1
+
+    file_improve_tracking.close()
+    cap.release()
+    cv2.destroyAllWindows()   
 
 if __name__ == "__main__":
     raw_path = "Code/Tracking/user_interaction/raw_output.txt"
@@ -113,3 +255,67 @@ if __name__ == "__main__":
 
     modified_data = edit_raw_output(raw_reader, edit_reader)  
     writer.write(modified_data)
+
+
+    """
+    detection_model = YOLO("C:/Users/julie/OneDrive - UCL/Master_2/Mémoire/Chimprec Dataset/Models/body/v8s/weights/best.pt")
+    input_video_path = "C:/Users/julie/Documents/Unif/Mémoire/tracking_video_test2.mp4"
+    output_text_file_path = "C:/Users/julie/Documents/Unif/Mémoire/tracking_improvement2.txt"
+    output_video_path = "C:/Users/julie/Documents/Unif/Mémoire/output_tracking2.mp4"
+
+    model_feature_extraction = torchreid.models.build_model(name='osnet_x1_0', num_classes=751, pretrained=True)
+    model_feature_extraction.eval()
+
+    # Paramètres de DeepSORT
+    max_cosine_distance = 0.5  # Distance max pour matcher un objet (plus bas = plus strict)
+    nn_budget = None  # Taille max du buffer pour le modèle d'appariement
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    # Initialiser DeepSORT
+    tracker = DeepSortTracker(metric)
+
+    colors = [
+        (120, 50, 99),
+        (180, 25, 16),
+        (73, 89, 176),
+        (200, 158, 18),
+        (199, 214, 152),
+        (181, 37, 229),
+        (118, 73, 165),
+        (136, 3, 53),
+        (40, 47, 142),
+        (246, 26, 168),
+        (33, 83, 190),
+        (151, 220, 243),
+        (156, 122, 217),
+        (173, 0, 128),
+        (61, 242, 230),
+        (37, 10, 125),
+        (64, 229, 201),
+        (64, 137, 49),
+        (136, 225, 85),
+        (146, 80, 77),
+        (255, 0, 0),
+        (0, 255, 0),
+        (0, 0, 255),
+        (255, 255, 0),
+        (0, 255, 255),
+        (255, 0, 255),
+        (255, 165, 0),
+        (255, 255, 255),
+        (0, 0, 0),
+        (128, 0, 0),
+        (0, 128, 0),
+        (128, 128, 0),
+        (0, 128, 128),
+        (128, 0, 128),
+        (255, 105, 180),
+        (255, 69, 0),
+        (34, 139, 34),
+        (70, 130, 180),
+        (255, 228, 225),
+        (218, 165, 32)
+    ]
+
+    perform_tracking(input_video_path, output_text_file_path, detection_model, tracker, 0.5, model_feature_extraction)
+    draw_bbox_from_file(output_text_file_path, input_video_path, output_video_path, colors)
+    """
